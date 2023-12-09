f878a02ba9eb5183aebabc6736bb4dc0ca032aea
Aleksey Vasilev <alexey.vasiljev@gmail.com>
2023-12-09 15:12:29 +0500

    Fix load_data command

--- backend/foodgramm_backend/recipes/management/commands/load_data.py
+++ backend/foodgramm_backend/recipes/management/commands/load_data.py
@@ -6,12 +6,12 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
+    """ Импорт таблицы с ингредиентами в базу данных """
     def handle(self, *args, **options):
         with open('../../data/ingredients.csv', encoding='utf-8') as file:
             file_reader = csv.reader(file)
             for row in file_reader:
                 name, measurement_unit = row
-                Ingredient.objects.get_or_create(
-                    name=name,
-                    measurement_unit=measurement_unit
-                )
+                Ingredient.objects.get_or_create(name=name,
+                                                 measurement_unit=measurement_unit)
+            print('Импорт успешно завершен')
