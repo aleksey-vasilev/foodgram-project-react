9245057193e443457916029f1a3dd87a7a738121
Алексей Васильев <alexey.vasiljev@gmail.com>
2023-12-11 19:09:03 +0500

    Change place of .env file

--- backend/foodgramm_backend/foodgramm_backend/settings.py
+++ backend/foodgramm_backend/foodgramm_backend/settings.py
@@ -8,7 +8,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR.parent / '.env')
+load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
